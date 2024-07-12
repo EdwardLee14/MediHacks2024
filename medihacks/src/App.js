737b02b46d96e45e64f4345f34980380a9959cc6
Edward Lee <7edwardlee@gmail.com>
2024-07-11 22:58:06 -0700

    implemented main page

--- medihacks/src/App.js
+++ medihacks/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor: "white", width: "100%", padding: "0.2px", boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)" }}>
+        <h1 style={{ marginLeft: "30px", fontSize: "35px", textAlign: "left" }}>
+          <span style={{ color: "black" }}>Trial & </span>
+          <span style={{ color: "#36d7b7", textDecoration: "underline" }}>Error</span>
+        </h1>
+        <button style={{ backgroundColor: "#36d7b7", border: "none", color: "white", padding: "10px 30px", borderRadius: "7px", fontSize: "18px", fontWeight: "bold", position: "absolute", top: "25px", right: "30px" }}>
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
